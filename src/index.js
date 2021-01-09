97898807a68a51a932c908a5d383e68a47677958
Nickolay Lyman <kolyalyman@gmail.com>
2021-01-09 14:08:54 +0200

    Выполнено дз

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './css/normalize.css';
+import './css/styles.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import './js/script.js';
